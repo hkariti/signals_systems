4ee3d4beb0aac7da95421fafb3b89068c93af5c3
Hagai Kariti <hkariti@gmail.com>
2017-11-11 19:12:32 +0200

    wet1: more questions

--- wet1/q4.m
+++ wet1/q4.m
@@ -2,17 +2,34 @@
 
 %% Part 1
 % Make new vector t1 using linespace
-
+t1 = linspace(-pi, pi, 100);
 % Make new vector t2 using syntax -
 %       t = t_first : del_t : t_last
+dt = 2*pi/100;
+t2 = -pi:dt:pi;
 %Compare them. Are they the identical?
 
 %% Part 2
-
+t = -pi:2*pi/9999:pi;
 % Make two sine-waves
-
+f1 = sin(1*t);
+f2 = sin(10.1*t);
 % Plot them and explain the results
+close all;
+figure();
+title("sin(1t) and sin(10.1t) between -pi and +pi");
+xlabel("Time");
+ylabel("Magnitude");
+hold();
+plot(t, f1, 'b');
+plot(t, f2, 'g');
 
 %% Part 3
 
 % Inner product of two sine-waves
+% calculate f1 and f2 for t1 (100 samples)
+f1_100 = sin(1*t1);
+f2_100 = sin(10.1*t1);
+% Calculate both inner product. We substitute delta_T/T0 = 1/N
+inner_product_100 = f1_100*f2_100' * 1/100;
+inner_product_10000 = f1*f2' * 1/10000;
--- wet1/q5.m
+++ wet1/q5.m
@@ -3,14 +3,31 @@
 % Fourier expansion of x(t) = t
 
 t = linspace(-pi,pi,10000);
+x = t;
 plot(t,x); hold all;
 
+T0 = 2*pi;
+N = length(t);
+M = 200
+% Start fourier series with l=0, fourier_function is 1
+fourier_approx = 1/N*x*exp(0*t)';
 for i = 1:M
-    
 %Calculate 2*M+1 coefficients -M,-(M-1),....-1,0,1,....M-1,M
-
+    fourier_function_p = exp(1j*2*pi/T0*i*t);
+    fourier_function_n = exp(-1j*2*pi/T0*i*t);
+    fourier_coeff_n = 1/N*x*fourier_function_n';
+    fourier_coeff_p = 1/N*x*fourier_function_p';
 %Calculate the Fourier series
-
-%plot Fourier series approx
-
+    fourier_approx = fourier_approx + fourier_coeff_n*fourier_function_n;
+    fourier_approx = fourier_approx + fourier_coeff_p*fourier_function_p;
 end
+% plot fourier approx and original function
+close all;
+figure();
+hold all;
+plot(t, x, 'b');
+plot(t, fourier_approx, 'g');
+legend("original function", "fo urier approx");
+
+% calculate diff between original function and fourier approx
+diff = 1/N * (x - fourier_approx)*(x - fourier_approx)'
