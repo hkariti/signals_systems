f48c576b384faba679fe3fc3be31d6f7e2b8830f
Hagai Kariti <hkariti@gmail.com>
2018-01-01 12:07:24 +0200

    q2: makeNote

--- wet2/makeNote.m
+++ wet2/makeNote.m
@@ -11,6 +11,11 @@ function x = makeNote(n, T, dt)
 %   x   <float array> which represents Temporal signal
 
 % Don't change the function signature
-
+f0 = 440;
+f = 2^(n/12)*f0;
+t = 0:dt:T;
+tau = 0.3;
+note = @(t) cos(2*pi*f*t).*exp(-3*t/tau);
+x = note(t);
 end
 
